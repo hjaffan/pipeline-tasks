ea40beb460df7f58c86417c3a2cced8ca5a6d9cd
hilaljaffan <hjaffan90@gmail.com>
2018-12-25 21:41:35 -0800

    Adding new params

--- generate-private-yml/task.sh
+++ generate-private-yml/task.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cat > config/private.yml <<EOL
+ROOT_DIR=$PWD
+cp -r source/. full-source/
+
+pushd ${ROOT_DIR}/full-source
+    cat > config/private.yml <<EOL
 ---
 blobstore:
   provider: s3
@@ -8,3 +12,4 @@ blobstore:
     access_key_id: ${AWS_ACCESS_KEY_ID}
     secret_access_key: ${AWS_SECRET_ACCESS_KEY}
 EOL
+popd
