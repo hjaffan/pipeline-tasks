1529ba0e0b5f8f52fc6d214f8180acf9f8d2f719
Hilal Jaffan <hj@jaffan.io>
2019-03-24 21:38:14 -0700

    Adding task to sync tables between two dynamodb accounts

---
+++ sync-dynamodb-tables/task.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import boto3
+import os
+
+# Region 
+
+source_region = os.getenv('SOURCE_REGION')
+source_aws_key = os.getenv('SOURCE_AWS_KEY')
+source_aws_secret = os.getenv('SOURCE_AWS_SECRET')
+source_table = os.getenv('SOURCE_TABLE')
+
+dest_region = os.getenv('DEST_REGION')
+dest_aws_key = os.getenv('DEST_AWS_KEY')
+dest_aws_secret = os.getenv('DEST_AWS_SECRET')
+dest_table = os.getenv('DEST_TABLE')
+
+
+dynamoclient = boto3.client('dynamodb', region_name=source_region,
+    aws_access_key_id=source_aws_key,
+    aws_secret_access_key=source_aws_secret)
+
+dynamotargetclient = boto3.client('dynamodb', region_name=dest_region,
+    aws_access_key_id=dest_aws_key,
+    aws_secret_access_key=dest_aws_secret)
+
+dynamopaginator = dynamoclient.get_paginator('scan')
+tabname=source_table
+targettabname=dest_table
+dynamoresponse = dynamopaginator.paginate(
+    TableName=tabname,
+    Select='ALL_ATTRIBUTES',
+    ReturnConsumedCapacity='NONE',
+    ConsistentRead=True
+)
+for page in dynamoresponse:
+    for item in page['Items']:
+        dynamotargetclient.put_item(
+            TableName=targettabname,
+            Item=item
+        )
